658a1d3d1d2ff8e2182df4979dce9d30ac0265c0
James Orozco <jamesorozcoh@gmail.com>
2024-08-22 17:10:27 -0500

    country app updated

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { HomePageComponent } from './pages/homePage/home-page.component';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AboutPageComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    LoadingSpinnerComponent,
+    SearchBoxComponent,
+    SidebarComponent,
+  ],
+  imports: [CommonModule, RouterModule],
+  exports: [
+    AboutPageComponent,
+    HomePageComponent,
+    LoadingSpinnerComponent,
+    SearchBoxComponent,
+    SidebarComponent,
+  ],
+})
+export class SharedModule {}
